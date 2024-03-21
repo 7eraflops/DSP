ecb24e06e6e54f2f82c2c44a3208f323c56cc7c3
Michał Charaszkiewicz <michal.charaszkiewicz@gmail.com>
2024-03-21 10:20:50 +0100

    minor updates

--- lab_02/problems.jl
+++ lab_02/problems.jl
@@ -1,36 +1,39 @@
 ##
 begin
-    using CairoMakie
     using Plots
     using Statistics
     using Random
 end
 
 ## problem 2.1
-begin
-    sampling_rate = 1000
-    sample_step = 1 / sampling_rate
-    amplitude = 2
-    frequency = 25
-    phase_shift = π / 4
-    first_sample_time = 0.25
-    sample_count = 256
-    t = range(; start=first_sample_time, step=sample_step, length=sample_count)
-    signal = amplitude * cos.((2 * π * frequency * t) .+ phase_shift)
-end
+sampling_rate = 1000
+sample_step = 1 / sampling_rate
+first_sample_time = 0.25
+sample_count = 256
+
+A = 2
+f₀ = 25
+ϕ = π / 4
+
+t = range(; start=first_sample_time, step=sample_step, length=sample_count)
+signal = A * cos.(2π * f₀ * t .+ ϕ)
+
+plot(signal)
 
 ## problem 2.2
-begin
-    sampling_rate = 2048
-    sample_step = 1 / sampling_rate
-    amplitude = 0.25
-    frequency = π / 2
-    phase_shift = π
-    first_sample_time = 5
-    last_sample_time = 10
-    t = range(; start=first_sample_time, stop=last_sample_time, step=sample_step)
-    signal = amplitude * exp.((im * 2 * π * frequency * t) .+ phase_shift)
-end
+sampling_rate = 2048
+sample_step = 1 / sampling_rate
+first_sample_time = 5
+last_sample_time = 10
+
+A = 0.25
+f₀ = π / 2
+ϕ = π
+
+t = range(; start=first_sample_time, stop=last_sample_time, step=sample_step)
+signal = A * exp.(im * (2π * f₀ * t .+ ϕ))
+p1 = plot(signal)
+plot(plot(real(signal)), plot(imag(signal)), layout=(2, 1), title=["real" "imaginary"], label=["re" "im"])
 
 ## problem 2.3
 function white_noise(n, power)
@@ -50,6 +53,6 @@ var(signal)
 
 ## problem 2.5
 function cw_rectangular(T, t)
-    
-    
+
+
 end
