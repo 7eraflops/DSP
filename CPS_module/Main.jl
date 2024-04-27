71c299f7c9b6f395b47d69591eeabd294a3d6586
Michał Charaszkiewicz <michal.charaszkiewicz@gmail.com>
2024-04-27 20:56:08 +0200

    all functions tested up to section 5.

--- CPS_module/Main.jl
+++ CPS_module/Main.jl
@@ -1,28 +1,128 @@
 ##
-include("CPS.jl")
-using Plots
-using .CPS
+begin
+    include("CPS.jl")
+    using Plots
+    using FFTW
+    using .CPS
+end
 
 ## continuous signals
-x = -2:0.001:2
+t = -2:0.001:2
 
-##
-plot(x, CPS.ramp_wave.(x))
-##
-plot(x, CPS.sawtooth_wave.(x))
-##
-plot(x, CPS.triangular_wave.(x))
-##
-plot(x, CPS.square_wave.(x))
-##
-plot(x, CPS.pulse_wave.(x, 0.75))
+## Impulses
 
+## problem 2.5
+plot(t, CPS.cw_rectangular.(t))
+## problem 2.6
+plot(t, CPS.cw_triangle.(t))
+## problem 2.7
+plot(t, CPS.cw_literka_M.(t))
+## problem 2.8
+plot(t, CPS.cw_literka_U.(t))
 
+## infinite bandwidth waves
+
+## problem 2.9
+plot(t, CPS.ramp_wave.(t))
+## problem 2.10
+plot(t, CPS.sawtooth_wave.(t))
+## problem 2.11
+plot(t, CPS.triangular_wave.(t))
+## problem 2.12
+plot(t, CPS.square_wave.(t))
+## problem 2.13
+plot(t, CPS.pulse_wave.(t, 0.75))
+## problem 2.14
+plot(t, CPS.impulse_repeater(cos, -π / 4, π / 4).(t))
+
+## finite bandwidth waves 
+
+## problem 2.15
+plot(t, CPS.ramp_wave_bl.(t; A=1.0, T=1.0, band=20.0))
+## problem 2.16
+plot(t, CPS.sawtooth_wave_bl.(t; A=1.0, T=1.0, band=20.0))
+## problem 2.17
+plot(t, CPS.triangular_wave_bl.(t; A=1.0, T=1.0, band=20.0))
+## problem 2.18
+plot(t, CPS.square_wave_bl.(t; A=1.0, T=1.0, band=20.0))
+## problem 2.19
+plot(t, CPS.pulse_wave_bl.(t; ρ=0.75, A=1.0, T=1.0, band=20.0,))
+## problem 2.20
+plot(t, CPS.impulse_repeater_bl(cos, 0, π / 2, 20).(t))
+## problem 2.21
+N = length(t)
+fs = 1 / 0.001
+freqs = [n * fs / N for n in 1:N]
+signal = CPS.rand_signal_bl(100, 300).(t)
+plot(t, signal)
+plot(freqs, abs2.(fft(signal)))
 
 ## discrete signals
 n = -100:1:100
 
-##
+## problem 2.22
 plot(n, CPS.kronecker.(n))
-##
+## problem 2.23
 plot(n, CPS.heaviside.(n))
+
+## windows
+N = 100
+
+## problem 2.24
+plot(CPS.rect.(N))
+## problem 2.25
+plot(CPS.triang.(N))
+## problem 2.26
+plot(CPS.hanning.(N))
+## problem 2.27
+plot(CPS.hamming.(N))
+## problem 2.28
+plot(CPS.blackman.(N))
+
+## signal parameters
+signal = randn(1024)
+
+## problem 3.1
+CPS.mean(signal)
+## problem 3.2
+CPS.peak2peak(signal)
+## problem 3.3
+CPS.energy(signal)
+## problem 3.4
+CPS.power(signal)
+## problem 3.5
+CPS.rms(signal)
+##problem 3.6
+plot(CPS.running_mean(signal, 10))
+##problem 3.7
+plot(CPS.running_energy(signal, 10))
+##problem 3.6
+plot(CPS.running_power(signal, 10))
+
+## sampling
+## problem 4.5
+f = t -> 5cos(t) - 2sin(3t) + cos(10t)
+m = -2:0.2:2
+s = f.(m)
+interpolated_signal = CPS.interpolate(m, s)
+t = -2:0.001:2
+plot(t, [f.(t), interpolated_signal.(t)]) # y1 - original signal y2 - interpolated signal
+plot(t, f.(t) - interpolated_signal.(t)) # error
+
+## quantization
+f = t -> 5cos(t) - 2sin(3t) + cos(10t)
+t = -2:0.001:2
+signal = f.(t)
+N = 16 # N-bit ADC
+L = LinRange(minimum(signal), maximum(signal), 2^N)
+
+## problem 5.1
+signal_quantized = CPS.quantize(L).(signal)
+noise = signal - signal_quantized
+plot(t, [signal, signal_quantized, noise])
+
+## problem 5.2
+CPS.SQNR(N)
+
+## problem 5.3
+CPS.SNR(CPS.power(signal), CPS.power(noise))
